20b274aa62fbe5ae35a58191ca1444dcf2da3bbf
Adrienpew <47397123+Adrienpew@users.noreply.github.com>
2019-04-10 13:17:31 -0400

    yes

--- admin.php
+++ admin.php
@@ -11,13 +11,34 @@
 
 <?php
     include ('db_connect.php');
+    $result = mysqli_query($conn, "SELECT * FROM users ORDER BY frat");
 ?>
 
 </head>
 
 <body>
 
-
+<table width='80%' border="0">
+    <tr bgcolor="teal">
+        <td>frat</td>
+        <td>id</td>
+        <td>hours</td>
+        <td>date</td>
+        <td>description</td>
+    </tr>
+    <?php
+    while($res = mysqli_fetch_array($result))
+    {
+        echo "<tr>";
+        echo "<td>".$res['frat']."</td>";
+        echo "<td>".$res['id']."</td>";
+        echo "<td>".$res['hours']."</td>";
+        echo "<td>".$res['date']."</td>";
+        echo "<td>".$res['description']."</td>";
+        echo "<td>"<a href=\"edit.php?id=$$$[id]\">Edit</a> | <a href=\"delete.php?id=$$$[id]" onChlick=\"return confirm('Are you sure you want to delete?')\">Delete</a><td>";
+    }
+    ?>
+</table>
 
 
 </body>
